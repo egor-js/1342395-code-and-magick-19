61aec90f6cf05cc9e63b5b94beb73f0e425ae250
EgorRadaevID1342395 <egor@serverice.ru>
2020-02-07 14:19:33 +0300

    commit1

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,42 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
-var wizards = [];
-wizards.length = 4;
+
+var wizards = new Array(4);
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COATS_COLOR = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COATS_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var openSetupButton = document.querySelector('.setup-open');
+openSetupButton.addEventListener('click', function () {
+  document.querySelector('.setup').classList.remove('hidden');
+});
+
+var closeSetupButton = document.querySelector('.setup-close');
+closeSetupButton.addEventListener('click', function () {
+  document.querySelector('.setup').classList.add('hidden');
+});
+
+var bigWizardCoat = document.querySelector('.setup-wizard').querySelector('.wizard');
+var indexCoatColor = 1;
+bigWizardCoat.addEventListener('click', function () {
+  bigWizardCoat.querySelector('.wizard-coat').style.fill = WIZARD_COATS_COLOR[indexCoatColor++];
+  if (indexCoatColor === WIZARD_COATS_COLOR.length) {
+    indexCoatColor = 0;
+  }
+});
+
+var bigWizardFireball = document.querySelector('.setup-fireball');
+var indexFireballColor = 1;
+bigWizardFireball.addEventListener('click', function () {
+  bigWizardFireball.style.fill = FIREBALL_COLOR[indexFireballColor++]; //querySelector('.fireball-color')
+  if (indexFireballColor === FIREBALL_COLOR.length) {
+    indexFireballColor = 0;
+  }
+});
 
 var generateName = function () {
   return Math.random() > 0.5 ? WIZARD_NAMES[Math.round(Math.random() * (WIZARD_NAMES.length - 1))] + ' ' + WIZARD_SURNAMES[Math.round(Math.random() * (WIZARD_SURNAMES.length - 1))] : WIZARD_SURNAMES[Math.round(Math.random() * (WIZARD_SURNAMES.length - 1))] + ' ' + WIZARD_NAMES[Math.round(Math.random() * (WIZARD_NAMES.length - 1))];
@@ -29,7 +57,7 @@ for (var i = 0; i < wizards.length; i++) {
 }
 
 var renderWizard = function (wizard) {
-  var str = wizard.coatColor;
+  var str = wizard.coatColor; // если цвет мантии волшебника задаётся с пробелом "rgb ( , , ,)"
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = str.replace(' ', '');
